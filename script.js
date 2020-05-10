a8d309d1297751513b1c154ec41f38ba8bf7f545
CzajnaExe <suberlakm@gmail.com>
2020-05-10 08:53:11 +0200

    Added dynamic copyright and style for active link

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Oj ty niegrzeczny tutaj się nie zalgąda");
+console.log("Oj ty niegrzeczny tutaj się nie zalgąda - ty hakierze :P");
+
+const copyritheElement = document.querySelector('.footer-copyright--js');
+const year = new Date().getFullYear();
+
+copyritheElement.innerHTML = `Copyright © ${year} Mateusz Suberlak - `;
