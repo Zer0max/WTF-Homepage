c51abc9beefc42c7e358aefc5e93510941ae4136
CzajnaExe <suberlakm@gmail.com>
2020-05-25 09:15:29 +0200

    Changes in page navigation and laoyout

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-console.log("Oj ty niegrzeczny tutaj się nie zalgąda - ty hakierze :P");
+function printHackerNoteInConsole() {
+    console.log("Oj ty niegrzeczny tutaj się nie zalgąda - ty hakierze :P");
+}
 
-const copyritheElement = document.querySelector('.footer-copyright--js');
-const year = new Date().getFullYear();
+function setCopyritheInformationIntoElement(fotterSelector) {
+    const copyritheElement = document.querySelector('.footer-copyright--js');
+    const year = new Date().getFullYear();
+    
+    copyritheElement.innerHTML = `Copyright © ${year} Mateusz Suberlak - `;
+}
 
-copyritheElement.innerHTML = `Copyright © ${year} Mateusz Suberlak - `;
+this.printHackerNoteInConsole();
+this.setCopyritheInformationIntoElement('.footer-copyright--js');
