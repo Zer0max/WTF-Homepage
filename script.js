f9d53b84e70dcebaaa3deac5dbd9d68d105914c0
CzajnaExe <suberlakm@gmail.com>
2020-05-25 22:41:05 +0200

    Added hamburger menu

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 function printHackerNoteInConsole() {
-    console.log("Oj ty niegrzeczny tutaj się nie zalgąda - ty hakierze :P");
+  console.log("Oj ty niegrzeczny tutaj się nie zalgąda - ty hakierze :P");
 }
 
 function setCopyritheInformationIntoElement(fotterSelector) {
-    const copyritheElement = document.querySelector('.footer-copyright--js');
-    const year = new Date().getFullYear();
-    
-    copyritheElement.innerHTML = `Copyright © ${year} Mateusz Suberlak - `;
+  const copyritheElement = document.querySelector(".footer-copyright--js");
+  const year = new Date().getFullYear();
+
+  copyritheElement.innerHTML = `Copyright © ${year} Mateusz Suberlak - `;
+}
+
+function initHammburgerMenu() {
+  const hammburegerElement = document.querySelector(".hammburger--js");
+  console.log(hammburegerElement);
+
+  hammburegerElement.addEventListener("click", () => {
+    const navigationElement = document.querySelector(".page-nav-header--js");
+    console.log(navigationElement);
+    navigationElement.classList.toggle("page-nav-header--open");
+  });
 }
 
 this.printHackerNoteInConsole();
-this.setCopyritheInformationIntoElement('.footer-copyright--js');
+this.setCopyritheInformationIntoElement(".footer-copyright--js");
+this.initHammburgerMenu();
